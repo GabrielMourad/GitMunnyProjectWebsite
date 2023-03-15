2b823ab5c7f7686c466670b2a9d1c008d3a3ecf5
Sergio Hernandez <ehernandez.sergio@gmail.com>
2023-03-15 15:23:32 -0700

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>GitMunny</h1>
+      <h2>Eden Rosales: Programmer</h2>
+      <h2>Dylan Moon: Expert Sudoku Solver</h2>
+      <h2>Aiesha Angeles: 2nd Expert Sudoku Solver</h2>
+      <h2>Gabriel Mourad: Moron</h2>
+      <h2>Sergio Hernandez: Cute</h2>
+      <h2>Chat gpt: Good friend, philanthropist, HR</h2>
+      <p>An app targeted toward younger audiences to gamify the concept
+        of budgeting to help to get them in the habit of making better
+        financial decisoins and finacial literacy. It's also real world experience for us developers
+        to practice managing a used application with real users.</p>
+        <h3>Major Features</h3>
+        <ul>
+          <li>Set a budget</li>
+          <li>Add a transition</li>
+          <li>Data persistence</li>
+          <li>User authentication</li>
+        </ul>
+        <h3>Technologies</h3>
+        <ul>
+          <li>React.js</li>
+          <li>React Native</li>
+          <li>Plaid API</li>
+          <li>Firebase</li>
+        </ul>
+        <h3>Skills Used</h3>
+        <ul>
+          <li>App development</li>
+          <li>Debugging</li>
+          <li>Project Management</li>
+          <li>User Authentication</li>
+          <li>UI Design</li>
+          <li>Database Management</li>
+          <li>Mathematics and Finance</li>
+          <li>API Integration</li>
+        </ul>
+        <img src="https://cdn.discordapp.com/attachments/1013580066309017603/1024099213019185212/Snapchat-866511628.jpg"/>
     </div>
   );
 }
