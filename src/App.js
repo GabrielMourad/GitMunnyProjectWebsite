f9baaa82e132c27cc666d8d26007fb345d8da66b
GabrielMourad <gabemrad@yahoo.com>
2023-03-16 15:57:32 -0700

    new changes for draft website

--- src/App.js
+++ src/App.js
@@ -3,44 +3,77 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>GitMunny</h1>
-      <h2>Eden Rosales: Programmer</h2>
-      <h2>Dylan Moon: Expert Sudoku Solver</h2>
-      <h2>Aiesha Angeles: 2nd Expert Sudoku Solver</h2>
-      <h2>Gabriel Mourad: Moron</h2>
-      <h2>Sergio Hernandez: Cute</h2>
-      <h2>Chat gpt: Good friend, philanthropist, HR</h2>
-      <p>An app targeted toward younger audiences to gamify the concept
-        of budgeting to help to get them in the habit of making better
-        financial decisoins and finacial literacy. It's also real world experience for us developers
-        to practice managing a used application with real users.</p>
-        <h3>Major Features</h3>
-        <ul>
-          <li>Set a budget</li>
-          <li>Add a transition</li>
-          <li>Data persistence</li>
-          <li>User authentication</li>
-        </ul>
-        <h3>Technologies</h3>
-        <ul>
-          <li>React.js</li>
-          <li>React Native</li>
-          <li>Plaid API</li>
-          <li>Firebase</li>
-        </ul>
-        <h3>Skills Used</h3>
-        <ul>
-          <li>App development</li>
-          <li>Debugging</li>
-          <li>Project Management</li>
-          <li>User Authentication</li>
-          <li>UI Design</li>
-          <li>Database Management</li>
-          <li>Mathematics and Finance</li>
-          <li>API Integration</li>
-        </ul>
-        <img src="https://cdn.discordapp.com/attachments/1013580066309017603/1024099213019185212/Snapchat-866511628.jpg"/>
+
+      <div className="git_title">
+        <h1 className="title_header">GitMunny</h1>
+        <h3>Cross Platform Budgetting Application</h3>
+      </div>
+
+      <div className="title">
+        <h1 className="title_header">Members</h1>
+      </div>
+
+      <div className="members">
+        <h2>Eden Rosales:<br/> Programmer</h2>
+        <h2>Dylan Moon:<br/>  Expert Sudoku Solver</h2>
+        <h2>Aiesha Angeles:<br/>  2nd Expert Sudoku Solver</h2>
+        <h2>Gabriel Mourad:<br/>  Moron</h2>
+        <h2>Sergio Hernandez:<br/>  Cute</h2>
+        <h2>Chat gpt:<br/>  Good friend, philanthropist, HR</h2>
+      </div>
+
+      <div className="title">
+        <h1 className="title_header">Introduction</h1>
+      </div>
+
+      <div className="introduction">
+        <p className = "intro_text">
+          An app targeted toward younger audiences to gamify the concept of budgeting to help to get them in the habit of making better financial decisoins and finacial literacy. It's also real world experience for us developers to practice managing a used application with real users. Did we mention finical literacy...
+        </p>
+      </div>
+     
+
+      <div className="title">
+        <h1 className="title_header">Major Features</h1>
+      </div>
+
+      <ul className = "tech_list">
+        <li>Set a budget</li>
+        <li>Add a transition</li>
+        <li>Data persistence</li>
+        <li>User authentication</li>
+      </ul>
+
+      <div className="title">
+        <h1 className="title_header">Technologies</h1>
+      </div>
+
+      <ul className = "tech_list">
+        <li>React.js</li>
+        <li>React Native</li>
+        <li>Plaid API</li>
+        <li>Firebase</li>
+      </ul>
+
+      <div className="title">
+        <h1 className="title_header">Skills Used</h1>
+      </div>
+
+      <ul className = "tech_list">
+        <li>App development</li>
+        <li>Debugging</li>
+        <li>Project Management</li>
+        <li>User Authentication</li>
+        <li>UI Design</li>
+        <li>Database Management</li>
+        <li>Mathematics and Finance</li>
+        <li>API Integration</li>
+      </ul>
+
+      <img src="https://cdn.discordapp.com/attachments/1013580066309017603/1024099213019185212/Snapchat-866511628.jpg" />
+
     </div>
+
   );
 }
 
